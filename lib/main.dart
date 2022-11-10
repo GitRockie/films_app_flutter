3a547b32bc69e0a29c992f498e7248b7505a8841
Roman Kostyuchenko <76057139+GitRockie@users.noreply.github.com>
2022-11-10 14:12:41 +0100

    :massage: Works on main
    :lock_with_ink_pen:  Firebase Aut create Anonimus

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:films_app_flutter/UI/routes/app_pages.dart';
 import 'package:films_app_flutter/UI/routes/app_routes.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
